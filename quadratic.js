bb00cec05496604ede1a71ecac224882b240674b
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-10-13 20:49:56 -0400

    Update quadratic.js

--- quadratic.js
+++ quadratic.js
@@ -1,8 +1,8 @@
 function quadratic(a, b, c) {
     let factors = []
     for(let i = (c - (Math.abs(c))); i <= Math.abs(c*2); i++) {
-// console.log(i)
-    // check if number is a factor
+// console.log(i) // << COMMENT 
+    // check if number is a factor  // << COMMENT 
     if(c % i == 0) {
         if(i+(c/i) == b){
         factors.push(['+ '+i,'+ '+(c/i)])
@@ -13,17 +13,10 @@ function quadratic(a, b, c) {
         }else if(i-(c/i) == b){
             factors.push(['- '+i,'- '+(c/i)])
         }
-        // console.log(i);
+        // console.log(i);  // << COMMENT 
     }
 }
 
-    // factors.forEach((q,d)=>{
-    //     let qqz = b-q 
-    //     if(factors.includes(qqz)){
-    //         console.log(factors[factors.indexOf(qqz)], q)
-    //     }
-    // })
-    // return factors
     console.log(factors)
     if(factors.length == 0) return `no factors found`
     let quaz = `(x ${factors[0][0]}) == 0 || (x ${factors[0][1]}) == 0`
@@ -35,7 +28,7 @@ function parse(equ) {
     return [(exp.split("x")[0] == '' ? 1 : exp.split("x")[0].split(" ").join("")) - 0, exp.split("x")[1] == '' ? 1 : (exp.split("x")[1].slice(2).split(" ").join("")) - 0, (exp.split("x")[2].split(" ").join("")) - 0]
 }
 
-// example x^2 - 7x + 12 = 0
+// example x^2 - 7x + 12 = 0  // << COMMENT 
 
 let equation = parse('x^2 + 5x + 14 = 0')
 
