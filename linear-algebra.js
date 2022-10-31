ed452b2441066650718a228eca7e890228f39fc1
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-10-31 14:01:41 -0400

    Update linear-algebra.js

--- linear-algebra.js
+++ linear-algebra.js
@@ -3,8 +3,19 @@ function vector2D(v) {
     return {Unitv: ([(v[0] / magnitude), (v[1] / magnitude)]), Unit: magnitude == 1, Magnitude: magnitude}
 }
 
+// vector2D([x,y])
+
 function vectorComponent(data){
     return [data.magnitude * Math.cos(data.theta), data.magnitude * Math.sin(data.theta)]
 }
 
-// vector2D[x,y]
+vectorComponent({magnitude, theta})
+
+function parametricVectorLine(v) {
+    let slope = [v[1], v[0]]
+    return {text: `{[1,0] + t[${v.toString()}] | t∈R}`, slope, LaTeX: `\{ {\hat{\textbf{\i}}} + t \begin{bmatrix} ${v[0]}
+\\ 
+${v[1]}
+\end{bmatrix}  | t \in \mathbb{R}\}
+`, altLaTeX: `\{ {\hat{\textbf{\i}}} + t  \vec{v} | t \in \mathbb{R}\}`}
+}
