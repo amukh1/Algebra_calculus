aac923420640d2903e6fd71178b30561cdd2b274
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-10-31 14:16:41 -0400

    Update linear-algebra.js

--- linear-algebra.js
+++ linear-algebra.js
@@ -3,16 +3,18 @@ function vector2D(v) {
     return {Unitv: ([(v[0] / magnitude), (v[1] / magnitude)]), Unit: magnitude == 1, Magnitude: magnitude}
 }
 
-// vector2D([x,y])
+// vector2D([x,y]) // {Unitv: [x,y], Unit: true/false, Magnitude: int}
 
 function vectorComponent(data){
     return [data.magnitude * Math.cos(data.theta), data.magnitude * Math.sin(data.theta)]
 }
 
-vectorComponent({magnitude, theta})
+// vectorComponent({magnitude, theta}) // [x,y] (component form)
 
 function parametricVectorLine(v) {
     let slope = `${v[1]}/${v[0]}`
     return {text: `{[1,0] + t[${v.toString()}] | t∈R}`, slope, LaTeX: `\\{ {\\hat{\\textbf{\\i}}} + t \\begin{bmatrix} ${v[0]}\n \\\\ \n ${v[1]} \\end{bmatrix}  | t \\in \\mathbb{R}\\}
-`, altLaTeX: `\\{ {\\hat{\\textbf{\\i}}} + t  \\vec{v} | t \\in \\mathbb{R}\\}`, equation: 'y=' + slope}
+`, altLaTeX: `\\{ {\\hat{\\textbf{\\i}}} + t  \\vec{v} | t \\in \\mathbb{R}\\}`, equation: 'y=' + slope + 'x'}
 }
+
+// parametricVectorLine([x,y]) // {text: {'[1,0] + t[x,y] | t∈R', [y,x], LateX: 'Latex code', altLaTeX: 'alternate Latex code', equation: 'y=mx'}
