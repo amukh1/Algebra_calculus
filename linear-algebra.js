9e4dd9d043b94a96bc1cf057cf8f87fb5957371a
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-10-31 13:33:27 -0400

    Update linear-algebra.js

--- linear-algebra.js
+++ linear-algebra.js
@@ -3,4 +3,8 @@ function vector2D(v) {
     return {Unitv: ([(v[0] / magnitude), (v[1] / magnitude)]), Unit: magnitude == 1, Magnitude: magnitude}
 }
 
+function vectorComponent(data){
+    return [data.magnitude * Math.cos(data.theta), data.magnitude * Math.sin(data.theta)]
+}
+
 // vector2D[x,y]
