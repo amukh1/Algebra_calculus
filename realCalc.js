f700a8bb676794bc17932aff8812513a53db8912
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-12-27 12:06:05 -0500

    Update realCalc.js

--- realCalc.js
+++ realCalc.js
@@ -1,8 +1,17 @@
-derive(dx, f, x) {
+function differentiate(dx, f, x) {
   return (f(x + dx) - f(x))/dx
+}
+
+function partial(fn, rsp){
+    return function(){
+        let f2 = fn.apply(null,arguments)
+        let arg2 = arguments
+        arg2[getParamNames(fn).indexOf(rsp)] += 0.1
+        return (fn.apply(null,arg2) - f2)/0.1
+    }
 }
 
 let s = (x) => x*x
 
 s(4) // ~16
-derive(0.0001, s, 4) // ~8
+differentiate(0.0001, s, 4) // ~8
